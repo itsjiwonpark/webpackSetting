359f778bbea01d640ee2fd56a27e6bbf8c0eab50
Jsunmin <tjsals7825@gmail.com>
2018-08-17 09:37:22 +0900

    setting

--- admin/src/app.js
+++ admin/src/app.js
@@ -8,29 +8,11 @@ console.log('app.js loaded');
 $(document).ready(() => {
   console.log('jquery works!')
 
-  let source = template.html();
-  let template = Handlebars
+  $(document).on('click', '.signin-btn', loginHandler);
 })
 let numbers = [1,2,3,4,5,6];
 console.log(template({numbers}));
 
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+loginHandler = () => {
+  console.log('hi!')
+};
